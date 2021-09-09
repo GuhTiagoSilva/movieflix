6278e6de4a40a263c77589eb33e0d5d6d5c983a9
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2021-09-09 08:18:51 -0300

    Repositories

---
+++ src/main/java/com/gustavo/movieflix/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.gustavo.movieflix.repositories;
+
+import com.gustavo.movieflix.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
